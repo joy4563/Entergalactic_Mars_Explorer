6bf519207d5bea6f191a693179855cc33f995d8d
Sabbbir <sabbir.ahmed1925@gmail.com>
2023-08-18 00:35:33 +0600

    Added Experimental and .json

---
+++ Experimental/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+for feature in data['features']:
+#     print(f"ID: {feature['id']}")
+#     print(f"Type: {feature['type']}")
+#     print(f"Name: {feature['name']}")
+#     print(f"Location: {feature['location']}")
+#     print(f"Description: {feature['description']}")
+#     print(f"Coordinates: Latitude {feature['coordinates']['latitude']}, Longitude {feature['coordinates']['longitude']}")
