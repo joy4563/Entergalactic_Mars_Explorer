8cb53e43eed64e4132730ad94aaf1510506aa7e8
ashik190141 <ashanmahmudashik@gmail.com>
2023-10-04 20:06:38 +0600

    registration done

---
+++ Registration/Registration.js
@@ -0,0 +1,38 @@
+function sendDataToDatabase(userInfo) {
+    fetch('http://localhost:5000/userRegister', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(userInfo)
+    })
+        .then(res => res.json())
+        .then(data => {
+            if (data.insertedId) {
+                alert("Registration Successful");
+                window.location.href = "../index.html";
+        }
+    })
+}
+
+
+document.getElementById('Login').addEventListener('click', function () {
+    let user_name = document.getElementById('name').value;
+    let user_phone = document.getElementById('phone').value;
+    let user_email = document.getElementById('email').value;
+    let user_age = document.getElementById('age').value;
+    let user_location = document.getElementById('location').value;
+    let user_password = document.getElementById('password').value;
+
+    const userInfo = {
+        userName: user_name,
+        userEmail: user_email,
+        userAge: user_age,
+        userPhone: user_phone,
+        userLocation: user_location,
+        userPassword: user_password
+    }
+
+    console.log(user_booking_info);
+    sendDataToDatabase(user_booking_info);
+})
